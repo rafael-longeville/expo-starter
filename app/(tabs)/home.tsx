2de62b67576a59d38dda09d7c3c1cdd6f568874b
Clément <91607630+disketflu@users.noreply.github.com>
2024-08-21 22:24:47 +0200

    update with transaction

--- app/(tabs)/home.tsx
+++ app/(tabs)/home.tsx
@@ -2,10 +2,12 @@ import { StyleSheet, Text, View, Button } from "react-native";
 import {
   useActiveAccount,
   useWalletBalance,
+  useSendTransaction
 } from "thirdweb/react";
 import { chain, client } from "@/constants/thirdweb";
 import { ScrollView } from "react-native";
-import { approve, transferFrom } from "thirdweb/extensions/erc20";
+import { getContract, prepareContractCall } from "thirdweb";
+import { toUnits } from "thirdweb/utils";
 
 
 export default function HomeScreen() {
@@ -21,11 +23,29 @@ export default function HomeScreen() {
   // console.log("balance", data?.displayValue, data?.symbol);
   // console.log("address", account?.address);``
 
+  const contract = getContract({
+    address: "0x0",
+    chain: chain,
+    client,
+  });
+
+  const { mutate: sendTx, data: transactionResult } = useSendTransaction();
+
+  const onClick = () => {
+    const transaction = prepareContractCall({
+        contract,
+        method: "function transfer(address to, uint256 value)",
+        params: ["0x4fc0C27D9F37dC94469e449CBe015df9A392c83e", toUnits("0.001", 18)],
+    });
+    sendTx(transaction);
+  };
+
   return (
     <ScrollView style={styles.scrollView}>
       <View style={styles.container}>
         <Text style={styles.blackSubtitle}>Bonjour {account?.address}</Text>
         <Text style={styles.title}>Vous avez {data?.displayValue} ETH</Text>
+        <Button title="Envoyer 0.001 ETH" onPress={onClick} />
       </View>
     </ScrollView>
   );
