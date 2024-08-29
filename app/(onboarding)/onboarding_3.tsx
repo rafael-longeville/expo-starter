b7f7693c2d66d6d385af2a0a993b046f16d151c7
Clément <91607630+disketflu@users.noreply.github.com>
2024-08-29 13:33:36 +0200

    custom uri with async storage

--- app/(onboarding)/onboarding_3.tsx
+++ app/(onboarding)/onboarding_3.tsx
@@ -1,17 +1,45 @@
 import { router } from "expo-router";
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { View, Text, StyleSheet, Pressable, Image } from "react-native";
 import { WebView } from 'react-native-webview';
 import { globalFonts } from "../styles/globalFonts";
 import {
   useActiveAccount,
 } from "thirdweb/react";
+import AsyncStorage from '@react-native-async-storage/async-storage';
 
 const Onboarding3: React.FC = () => {
   const account = useActiveAccount();
-  if(account) {
-    console.log(account.address)
-  }
+  const [onboardingValue, setOnboardingValue] = useState<string | null>(null);
+  const [onboardingMethod, setOnboardingMethod] = useState<string | null>(null);
+  const [customUri, setCustomUri] = useState<string | null>(null);
+
+  useEffect(() => {
+    const loadOnboardingData = async () => {
+      try {
+        const value = await AsyncStorage.getItem('onboardingValue');
+        const method = await AsyncStorage.getItem('onboardingMethod');
+        setOnboardingValue(value);
+        setOnboardingMethod(method);
+
+        if (account?.address) {
+          let uri = 'https://global-stg.transak.com/?apiKey=ec807ee4-b564-4b2a-af55-92a8adfe619b&network=arbitrum&paymentMethod=credit_debit_card&visaMasterCard=true&defaultCryptoCurrency=USDC&productsAvailed=BUY&walletAddress=' + account.address;
+          
+          if (method === 'onRamp') {
+            uri = 'https://global-stg.transak.com/?apiKey=ec807ee4-b564-4b2a-af55-92a8adfe619b&network=arbitrum&paymentMethod=credit_debit_card&visaMasterCard=true&defaultCryptoCurrency=USDC&productsAvailed=BUY&fiatCurrency=EUR&walletAddress=' + account.address + '&fiatAmount=' + onboardingValue;
+          }
+
+          setCustomUri(uri);
+          console.log(uri)
+        }
+      } catch (error) {
+        console.log("Error loading onboarding data from AsyncStorage", error);
+      }
+    };
+
+    loadOnboardingData();
+  }, [account, onboardingMethod, onboardingValue]);
+
   return (
     <View style={styles.container}>
       <View style={styles.containercompte}>
@@ -25,9 +53,9 @@ const Onboarding3: React.FC = () => {
       <Text style={globalFonts.subtitle}>
         Placez vos fonds dans un des compte rémunéré via carte bancaire, Apple Pay ou Google Pay.
       </Text>
-      {account?.address && <WebView
+      {customUri && <WebView
         style={styles.webview}
-        source={{ uri: 'https://global-stg.transak.com/?apiKey=ec807ee4-b564-4b2a-af55-92a8adfe619b&network=arbitrum&paymentMethod=credit_debit_card&visaMasterCard=true&defaultCryptoCurrency=USDC&productsAvailed=BUY&walletAddress=' + account.address }}
+        source={{ uri: customUri }}
       />}
     </View>
   );
@@ -55,11 +83,11 @@ const styles = StyleSheet.create({
     backgroundColor: '#13293D',
     flexDirection: 'row',
     alignItems: 'center',
-    paddingLeft: 20, // Adjust padding to the left as needed
-    marginBottom: 30
+    paddingLeft: 20,
+    marginBottom: 30,
   },
   icon: {
-    marginRight: 10, // Add some margin between icon and text
+    marginRight: 10,
   },
   textcompte: {
     flexDirection: 'row',
