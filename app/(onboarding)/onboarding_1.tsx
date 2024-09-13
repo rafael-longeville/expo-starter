0895756b31a058a5d45039a2ca7321b542399ac9
Rafael Longeville <rafael.longeville.pro@gmail.com>
2024-09-13 12:42:44 +0200

    push to production branch in eas

--- app/(onboarding)/onboarding_1.tsx
+++ app/(onboarding)/onboarding_1.tsx
@@ -172,7 +172,7 @@ const Onboarding1: React.FC = () => {
     <View style={styles.container}>
       <View>
         <Text style={globalFonts.subtitle}>
-          Hello from expo automatic update 2
+          Hello from expo automatic update 3
         </Text>
         <Text style={globalFonts.title}>{t("pages.onboarding_1.title")}</Text>
       </View>
