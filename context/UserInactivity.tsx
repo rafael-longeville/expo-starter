f8cf202dcf79fcb03b6a36ee03812a95d6ba22c1
Rafael Longeville <rafael.longeville.pro@gmail.com>
2024-09-18 08:10:11 +0200

    5 sec

--- context/UserInactivity.tsx
+++ context/UserInactivity.tsx
@@ -43,7 +43,7 @@ export const UserInactivityProvider = ({
       );
       const elapsed = Date.now() - parseInt(startTime || "0");
       console.log("Elapsed time:", elapsed);
-      if (elapsed > 1000 && account && wallet) {
+      if (elapsed > 5000 && account && wallet) {
         console.log("User has been inactive for more than 1000ms");
         Alert.alert(
           "You have been inactive for more than 5 seconds, so you have to login again"
