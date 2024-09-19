3d8327e2255b0033221f0ed21c3af4b153789768
Rafael Longeville <rafael.longeville.pro@gmail.com>
2024-09-19 15:18:53 +0200

    38.0.2

--- app/(tabs)/account.tsx
+++ app/(tabs)/account.tsx
@@ -221,7 +221,7 @@ function Account({
               textAlign: "center",
             }}
           >
-            Version {CONFIG.expo.ios.buildNumber}.0.1
+            Version {CONFIG.expo.ios.buildNumber}.0.2
           </Text>
         </View>
       </ScrollView>
