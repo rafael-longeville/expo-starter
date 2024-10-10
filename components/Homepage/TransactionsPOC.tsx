e59fa5ae73097899f565e18f96fbe78fa533cc6d
Clément <91607630+disketflu@users.noreply.github.com>
2024-10-11 01:40:01 +0200

    Update TransactionsPOC.tsx

--- components/Homepage/TransactionsPOC.tsx
+++ components/Homepage/TransactionsPOC.tsx
@@ -19,56 +19,97 @@ export default function TransactionPOC({
   const [transactionObject, setTransactionObject] = useState<any>(null);
   const [signedMessage, setSignedMessage] = useState<string | null>(null);
 
-  const contractApprove = getContract({
+  const contractART = getContract({
     client,
     chain: chain,
     address: "0x070E6A0e832401547a82AF5D6E2360438cf450cB"
   });
 
-  const contractStake = getContract({
+  const contractRadaoStaker = getContract({
     client,
     chain: chain,
     address: "0x18406a4C275925fF2Bf5Fdd0616C2872Ff44202E"
   });
 
-  const transactions = [
+  const contractTrUSDC = getContract({
+    client,
+    chain: chain,
+    address: "0xa1Ebb6CcECDFE0CbC0aaE08E73917AA8E534a7Ec"
+  });
+
+  const transactionBuyArtWithTrUsdc = [
+    // Send trUSDC test token then receive ART in another tx
     prepareContractCall({
-      contract: contractApprove,
+      contract: contractTrUSDC,
+      method: "function transfer(address to, uint256 value)",
+      params: ["0x7Bfe5d2746D51342DD3a1F864D66B1bD74C5a0eE", toUnits("0.001", 6)],
+    })
+  ];
+
+  const transactionApproveAndStakeArt = [
+    // Approve ART
+    prepareContractCall({
+      contract: contractART,
       method: "function approve(address spender, uint256 value)",
-      params: ["0x18406a4C275925fF2Bf5Fdd0616C2872Ff44202E", BigInt(115792089237316195423570985008687907853269984665640564039457584007913129639935)],
+      params: ["0x18406a4C275925fF2Bf5Fdd0616C2872Ff44202E", BigInt(Number.MAX_SAFE_INTEGER)],
     }),
+    // Stake ART
     prepareContractCall({
-      contract: contractStake,
+      contract: contractRadaoStaker,
       method: "function stake(address token, uint256 tokenValue, address stArtsRecipient)",
-      params: ["0x070E6A0e832401547a82AF5D6E2360438cf450cB", toUnits("0.0033808", 6), account?.address],
+      params: ["0x070E6A0e832401547a82AF5D6E2360438cf450cB", toUnits("0.001", 6), account?.address],
     }),
+  ];
+
+  const transactionApproveAndUnstakeArt = [
+    // Approve ART
+    prepareContractCall({
+      contract: contractART,
+      method: "function approve(address spender, uint256 value)",
+      params: ["0x18406a4C275925fF2Bf5Fdd0616C2872Ff44202E", BigInt(Number.MAX_SAFE_INTEGER)],
+    }),
+    // Unstake ART
+    prepareContractCall({
+      contract: contractRadaoStaker,
+      method: "function unstake(address token, uint256 tokenValue, address stArtsRecipient)",
+      params: ["0x070E6A0e832401547a82AF5D6E2360438cf450cB", toUnits("0.001", 6), account?.address],
+    }),
+  ];
+
+  const transactionSellArt = [
+    //Sell ART (transfer then receive USDC)
+    prepareContractCall({
+      contract: contractART,
+      method: "function transfer(address to, uint256 value)",
+      params: ["0x7Bfe5d2746D51342DD3a1F864D66B1bD74C5a0eE", toUnits("0.001", 6)],
+    })
   ];
 
   const onClick = async () => {
     if (account) {
-      try {
-        // Send the transaction
-        const tx =  await sendBatchTransaction({
-          transactions,
-          account
-        });
+      // try {
+      //   // Send the transaction
+      //   const tx =  await sendBatchTransaction({
+      //     transactions,
+      //     account
+      //   });
 
-        refetch();
-        setTransactionObject(tx);
-        console.log(tx)
-        const txHash = tx?.transactionHash; // Extract the transaction hash
-        console.log("Transaction hash:", txHash);
+      //   refetch();
+      //   setTransactionObject(tx);
+      //   console.log(tx)
+      //   const txHash = tx?.transactionHash; // Extract the transaction hash
+      //   console.log("Transaction hash:", txHash);
 
-        if (txHash) {
-          Alert.alert("Transaction Successful", `Transaction Hash: ${txHash}`);
-        }
-      } catch (error) {
-        console.error("Transaction error:", error as Error);
-        Alert.alert(
-          "Transaction Failed",
-          `Error: ${(error as Error).message || error}`
-        );
-      }
+      //   if (txHash) {
+      //     Alert.alert("Transaction Successful", `Transaction Hash: ${txHash}`);
+      //   }
+      // } catch (error) {
+      //   console.error("Transaction error:", error as Error);
+      //   Alert.alert(
+      //     "Transaction Failed",
+      //     `Error: ${(error as Error).message || error}`
+      //   );
+      // }
     } else {
       Alert.alert("Account Not Found", "Please connect your account.");
     }
