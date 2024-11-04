d2f6b62f3cb42b673d878c1114810166b041c25a
Rafael Longeville <rafael.longeville.pro@gmail.com>
2024-11-04 18:54:44 +0100

    Push Onboarding_4 pixel perfect

--- app/i18n.ts
+++ app/i18n.ts
@@ -10,36 +10,36 @@ import fr from "./locales/fr.json";
 
 // Get the preferred locale
 const locales = getLocales();
-const preferredLocale = locales[0]?.languageCode || "en"; // Fallback to 'en' if locale is not available
+const preferredLocale = locales[0]?.languageCode || "fr"; // Fallback to 'en' if locale is not available
 
 i18n
   .use(initReactI18next) // Passes i18n instance to react-i18next.
   .init({
     resources: {
-      en: { translation: en },
+      // en: { translation: en },
       fr: { translation: fr },
     },
-    lng: preferredLocale, // Set the initial language based on the device's locale
+    lng: "fr", // Set the initial language based on the device's locale
     fallbackLng: "en", // Fallback language
     interpolation: {
       escapeValue: false, // React already escapes values
     },
   });
 
-// Check if "selectedLanguage" is already set in AsyncStorage
-const setInitialLanguage = async () => {
-  const selectedLanguage = await AsyncStorage.getItem("selectedLanguage");
-  if (!selectedLanguage) {
-    await AsyncStorage.setItem("selectedLanguage", preferredLocale || "en");
-    if (preferredLocale == "fr") {
-      await AsyncStorage.setItem("selectedCurrency", "euro");
-    } else await AsyncStorage.setItem("selectedCurrency", "dollar");
-  } else {
-    i18n.changeLanguage(selectedLanguage);
-  }
-};
+// // Check if "selectedLanguage" is already set in AsyncStorage
+// const setInitialLanguage = async () => {
+//   const selectedLanguage = await AsyncStorage.getItem("selectedLanguage");
+//   if (!selectedLanguage) {
+//     await AsyncStorage.setItem("selectedLanguage", preferredLocale || "en");
+//     if (preferredLocale == "fr") {
+//       await AsyncStorage.setItem("selectedCurrency", "euro");
+//     } else await AsyncStorage.setItem("selectedCurrency", "dollar");
+//   } else {
+//     i18n.changeLanguage(selectedLanguage);
+//   }
+// };
 
-// Call the function to set the initial language
-setInitialLanguage();
+// // Call the function to set the initial language
+// setInitialLanguage();
 
 export default i18n;
