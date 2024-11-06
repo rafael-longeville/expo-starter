60422fa37236a873fdd543d27d972f1c1f6ad50b
Rafael Longeville <rafael.longeville.pro@gmail.com>
2024-11-06 11:20:41 +0100

    Push 105

--- app/(onboarding)/onboarding_1.tsx
+++ app/(onboarding)/onboarding_1.tsx
@@ -7,7 +7,6 @@ import CreateWithPasskey from "@/components/SignInSignUp/CreateWithPasskey";
 import AsyncStorage from "@react-native-async-storage/async-storage";
 import { Link } from "expo-router";
 import { ActivityIndicator } from "react-native-paper";
-import ConnectWithGoogle from "@/components/SignInSignUp/ConnectWithGoogle";
 import * as Sentry from "@sentry/react-native";
 import { useRouter } from "expo-router";
 import { getLocales } from "expo-localization";
