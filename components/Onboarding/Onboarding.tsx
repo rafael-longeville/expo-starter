496d1f46f5f84081ec9dafbac984e038b36b0efc
Clément <91607630+disketflu@users.noreply.github.com>
2024-11-06 14:35:50 +0100

    fix modals height

--- components/Onboarding/Onboarding.tsx
+++ components/Onboarding/Onboarding.tsx
@@ -200,7 +200,6 @@ const styles = StyleSheet.create({
     justifyContent: "center",
     alignItems: "center",
     height: "100%",
-    backgroundColor: "#333333",
   },
   backgroundImage: {
     flex: 1,
