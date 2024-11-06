bf64ec914f704a63991b385127e04db2c84fc5d5
Clément <91607630+disketflu@users.noreply.github.com>
2024-11-06 13:59:24 +0100

    Update Onboarding.tsx

--- components/Onboarding/Onboarding.tsx
+++ components/Onboarding/Onboarding.tsx
@@ -200,6 +200,7 @@ const styles = StyleSheet.create({
     justifyContent: "center",
     alignItems: "center",
     height: "100%",
+    backgroundColor: "#333333",
   },
   backgroundImage: {
     flex: 1,
